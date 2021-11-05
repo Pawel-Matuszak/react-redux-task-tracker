291cdccffdf7d0b39c7aa6145dc86504f9d2af6f
pawelmatuszak <56409425+Pawel-Matuszak@users.noreply.github.com>
2021-11-05 19:57:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,48 +5,12 @@ import AddTask from "./components/AddTask"
 import { useDispatch, useSelector, useStore} from "react-redux"
 
 function App() {
-//   const [tasks, setTasks] = useState([
-//     {
-//         id: 1,
-//         title: "Title 1",
-//         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-//         reminder: false,
-//     },
-//     {
-//         id: 2,
-//         title: "Title 2",
-//         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-//         reminder: false,
-//     },
-//     {
-//         id: 3,
-//         title: "Title 3",
-//         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-//         reminder: true,
-//     }
-// ])
 const items = useSelector(state=>state.items);
 const [displayForm, setForm] = useState(false);
 
 const toggleForm = () =>{
   setForm(!displayForm);
 }
-// const addTask = (task) => {
-//   const id = Math.floor(Math.random() * 1000) + 1
-//   const newTask = {id, ...task}
-//   setTasks([...tasks, newTask]);
-// }
-
-// const deleteTask = (id) =>{
-//   setTasks(tasks.filter(task=>task.id!==id))
-// }
-
-// const toggleReminder = (id) => {
-//   setTasks(tasks.map(task=>
-//     task.id===id ? {...task, reminder: !task.reminder} : task
-//   ))
-// }
-
 
 
 return (
